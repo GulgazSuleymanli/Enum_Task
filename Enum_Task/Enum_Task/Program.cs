e91f8da80c8c7be418d4235d068b1147fc3d73d8
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2023-10-19 05:33:24 +0400

    Enum_Tasks Completed

---
+++ Enum_Task/Enum_Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using Enum_Task.Enums;
+
+namespace Enum_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChangeCurrency(Currency.TRY, 123.45));
+        }
+
+        public static double ChangeCurrency(Currency currency, double azn)
+        {
+            double newValue;
+            switch(currency)
+            {
+                case Currency.Usd:
+                    newValue = azn * 0.59;
+                    return newValue;
+
+                case Currency.Eur:
+                    newValue = azn * 0.56;
+                    return newValue;
+
+                case Currency.TRY:
+                    newValue = azn * 16.48;
+                    return newValue;
+
+                case Currency.Rub:
+                    newValue = azn * 57.35;
+                    return newValue;
+
+                default:
+                    return -1;
+            }
+        }
+    }
+}
